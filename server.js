a14340b7913770028d03ee3dcd3435101b382a62
K0JICODES <k0jikodes@gmail.com>
2025-08-02 22:10:02 -0400

    fix: update Google OAuth callback URL to use environment variable
    - Changed the Google OAuth callback URL to utilize the environment variable for better flexibility in deployment configurations, ensuring the application can easily switch between development and production environments.

--- server.js
+++ server.js
@@ -96,8 +96,7 @@ passport.deserializeUser((user, done) => {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    //callbackURL: "http://localhost:3000/auth/google/callback" 
-    callbackURL: "https://typing-game.azurewebsites.net/auth/google/callback"
+    callbackURL: process.env.GOOGLE_CALLBACK_URL
 },
 async function(accessToken, refreshToken, profile, done) {
     // No MSSQL logic, just pass 
